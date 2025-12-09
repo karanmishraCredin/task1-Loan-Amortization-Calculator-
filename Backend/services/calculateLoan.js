278589aef9b309d67962db892b2f49c5a0da068c
karanmishraCredin <karan.mishra@credin.in>
2025-12-09 17:29:43 +0530

    fix the bug in table

--- Backend/services/calculateLoan.js
+++ Backend/services/calculateLoan.js
@@ -2,9 +2,7 @@ export default function calculateLoan(principal, yearlyRate, years) {
     const P = Number(principal);
     const r = Number(yearlyRate) / 100 / 12; 
     const n = Number(years) * 12;
-
     let monthlyPayment;
-
     if (r === 0) {
         monthlyPayment = P / n;
     } else {
@@ -13,30 +11,23 @@ export default function calculateLoan(principal, yearlyRate, years) {
             (Math.pow(1 + r, n) - 1);
     }
     monthlyPayment = Number(monthlyPayment.toFixed(2));
-
     const schedule = [];
     let balance = P;
-
     for (let i = 1; i <= n; i++) {
         const interestPayment = balance * r;
         let principalPayment = monthlyPayment - interestPayment;
-
         if (i === n) {
             principalPayment = balance;
         }
-
         balance -= principalPayment;
-
         schedule.push({
             paymentNumber: i,
             interestPaid: Number(interestPayment.toFixed(2)),
             principalPaid: Number(principalPayment.toFixed(2)),
             remainingBalance: Number(balance < 0 ? 0 : balance.toFixed(2))
         });
-
         if (balance <= 0) balance = 0;
     }
-
     return {
         principal: P,
         yearlyRate,
--- Frontend/src/pages/Tables/BasicTable.jsx
+++ Frontend/src/pages/Tables/BasicTable.jsx
@@ -13,33 +13,40 @@ import { useEffect } from 'react';
 export default function BasicTable() {
     const { state, setState } = useMain()
     useEffect(() => {
-        console.log("state in table-> ",state)
+        console.log("state in table-> ", state)
     }, [state])
     return (
-        <TableContainer sx={{overflow:'scroll',height:'90vh'}} component={Paper}>
-            <Table sx={{ minWidth: 650 ,overflow:'scroll' }} aria-label="simple table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell align="right">interestPaid </TableCell>
-                        <TableCell align="right">paymentNumber</TableCell>
-                        <TableCell align="right">principalPaid</TableCell>
-                        <TableCell align="right">remainingBalance</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {state?.schedule?.map((row) => (
-                        <TableRow
-                            key={row.interestPaid + Date.now()}
-                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                        >
-                            <TableCell align="right">{row.interestPaid}</TableCell>
-                            <TableCell align="right">{row.paymentNumber}</TableCell>
-                            <TableCell align="right">{row.principalPaid}</TableCell>
-                            <TableCell align="right">{row.remainingBalance}</TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+        <>
+            {
+                state?.schedule.length > 0 ?
+
+                    <TableContainer sx={{ overflow: 'scroll', height: '90vh' }} component={Paper}>
+                        <Table sx={{ minWidth: 650, overflow: 'scroll' }} aria-label="simple table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell align="right">interestPaid </TableCell>
+                                    <TableCell align="right">paymentNumber</TableCell>
+                                    <TableCell align="right">principalPaid</TableCell>
+                                    <TableCell align="right">remainingBalance</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {state?.schedule?.map((row) => (
+                                    <TableRow
+                                        key={row.interestPaid + Date.now()}
+                                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                    >
+                                        <TableCell align="right">{row.interestPaid}</TableCell>
+                                        <TableCell align="right">{row.paymentNumber}</TableCell>
+                                        <TableCell align="right">{row.principalPaid}</TableCell>
+                                        <TableCell align="right">{row.remainingBalance}</TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                    : <h1 style={{color:'black'}} >No Data Found !</h1>}
+        </>
+
     );
 }
